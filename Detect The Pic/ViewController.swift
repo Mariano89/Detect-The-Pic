cc86f77b0f69cee94bb589f18be049b00db17140
Mariano Echegoyen <mariano.echegoyen@dng.com>
2018-01-02 17:21:21 -0800

    detect contents of uploaded image using CoreML

--- Detect The Pic/ViewController.swift
+++ Detect The Pic/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let resnetModel = Resnet50()
+    var imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func processPic(image: UIImage) {
+        if let model = try? VNCoreMLModel(for: resnetModel.model) {
+            let request = VNCoreMLRequest(model: model, completionHandler: { (request, error) in
+                if let results = request.results as? [VNClassificationObservation] {
+                    for result in results {
+                        print("ID: \(result.identifier) Confidence: \(result.confidence)")
+                    }
+                }
+            })
+            
+            if let imageData = UIImageJPEGRepresentation(image, 1.0) {
+                let handler = VNImageRequestHandler(data: imageData, options: [:])
+                try? handler.perform([request])
+            }
+        }
     }
-
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = image
+            processPic(image: image)
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func photosTapped(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
